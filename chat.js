82b6a4e2006ac00ab5e48e5d98d2026a9c624e9c
jfoucher <jfoucher@student.42.fr>
2024-03-23 18:40:10 +0100

    chat: chatbox closing when clicking on the cross and opening when clicking on a player

--- chat.js
+++ chat.js
@@ -1,8 +1,27 @@
 const chatBoxCloseButton = document.getElementById("c-chat-box-close-button");
 const chatBox = document.getElementById("c-chat-box");
+const chatBoxHeaderPlayerName = document.querySelector("#c-chat-box-header h3");
+
+const players = [
+    {
+        id: "PlayerA#1234",
+        name: "PlayerA"
+    },
+    {
+        id: "PlayerB#1564",
+        name: "PlayerB"
+    }
+];
+
+const openChatBox = (receivingPlayerId) => {
+    chatBox.classList.add("hidden");
+    const receivingPlayer = players.find((player) => player.id === receivingPlayerId);
+    console.log(receivingPlayer);
+    chatBoxHeaderPlayerName.textContent = receivingPlayer.name;
+    chatBox.classList.remove("hidden");
+};
 
 const closeChatBox = () => {
-    console.log("close");
     chatBox.classList.add("hidden");
 };
 
