759efa4ae9cfa60a9af10091fc01577d1852ec71
jfoucher <jfoucher@student.42.fr>
2024-03-30 15:50:27 +0100

    chat: search friend list feature added

--- chat.js
+++ chat.js
@@ -5,11 +5,25 @@ const chatMessagesDiv = document.getElementById("c-chat-messages");
 const chatInputContent = document.querySelector("#c-chat-input input");
 const sendMessageButton = document.querySelector("#c-chat-input button");
 const friendsList = document.getElementById("c-friends-list");
+const friendsListSearchInput = document.querySelector("#c-search-friend-list input");
+const friendsListSearchButton = document.querySelector("#c-search-friend-list button");
 
 const friends = [
     {
         id: "#1234",
         name: "Jack"
+    },
+    {
+        id: "#1254",
+        name: "Samantha"
+    },
+    {
+        id: "#1267",
+        name: "Arthur"
+    },
+    {
+        id: "#1289",
+        name: "Pierre"
     },
     {
         id: "#1564",
@@ -24,14 +38,26 @@ const messages = [
     }
 ];
 
-const renderUserList = () => {
-    const friendsHTML = friends.map((user) => {
+const renderFriendList = (list = friends) => {
+    const friendsHTML = list.map((user) => {
         return `<div id="c-list-user-${user.id}" class="c-user" onclick="openChatBox('${user.id}')">
                     <div class="user-avatar"><img src="icon/default.jpg" alt="profile-picture"></div>
                     <div class="user-name">${user.name}</div>
                 </div>`;
     }).join("");
     friendsList.innerHTML = friendsHTML;
+};
+
+const searchFriendList = () => {
+    if (friendsListSearchInput.value === "") {
+        renderFriendList();
+        return;
+    }
+    const regex = /([A-Za-z]+)/g;
+    const parsedSearchInput = friendsListSearchInput.value.match(regex).join("");
+    console.log(parsedSearchInput);
+    const matchlist = friends.filter((friend) => friend.name.includes(parsedSearchInput));
+    renderFriendList(matchlist);
 };
 
 const renderMessages = () => {
@@ -63,12 +89,18 @@ const closeChatBox = () => {
     chatBox.classList.add("hidden");
 };
 
-renderUserList();
+renderFriendList();
 
+friendsListSearchButton.addEventListener("click", searchFriendList);
 chatBoxCloseButton.addEventListener("click", closeChatBox);
 sendMessageButton.addEventListener("click", sendMessage);
 chatInputContent.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
         sendMessage();
+    }
+});
+friendsListSearchInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        searchFriendList();
     }
 });
