61bf81a99ddb77f29b6333d9b16ea5918fc3168f
jfoucher <jfoucher@student.42.fr>
2024-04-23 09:19:53 +0200

    chat: testing backend

--- webSocket/src/js/client.js
+++ webSocket/src/js/client.js
@@ -35,7 +35,8 @@ const   client = {
 
 // user.id = '#' + ((Math.floor(Math.random() * 6)) * 2);
 user.id = '#' + 10 + ((Math.floor(Math.random() * 6)) * 2);
-user.name = 'xuluu' + user.id + 'abc';
+//user.name = 'xuluu' + user.id + 'abc';
+user.name = "Antoine";
 user.status = 'connection';
 user.avatar = "../../img/avatar/avatar2.jpg";
 client.inforUser = user;
@@ -46,6 +47,85 @@ class   dataToServer {
         this.content = c,
         this.destination = d
     }
+}
+
+async function loginTestUser()                      //Fonction asynchrone, elle s'execute en parallele et peut se terminer plus tard, pour utilisé son contenu il faut utilisé "await"
+{
+    const test_user = {
+        username: client.inforUser.name,
+        password: "abcd*1234"
+    };
+    console.log("Starting loginTestUser");
+    console.log(test_user);
+        const r = await fetch("http://127.0.0.1:8000/api/v1/auth/login", {     //adresse ip:port/ path du backend pour enregistrer un utilisateur
+            method: "POST",                                                       //Type de méthode utilisé pour le fetch
+            body: JSON.stringify(test_user),                                       //Convertie une string en .JSON
+            headers: {                                                            //Informations sur le contenu envoyé
+                "Accept": "application/json",
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(response =>  {
+            console.log("status =" + response.status);
+            console.log(JSON.stringify(test_user));
+            if (response.status >= 200 && response.status < 300)
+            {
+                console.log("login success");
+            }
+            else if (response.status === 400)
+            {
+                console.log("error");
+                return response.json();
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("On affiche le contenu qui est convertie depuis un JSON grace a response.json(): " + JSON.stringify(data));
+            localStorage.setItem("refresh", "refreshvalue");
+            localStorage.setItem("token", "tokenvalue");
+        })
+        .catch(error => {
+            console.error("loginTestUser :", error);
+        });
+}
+
+async function registerTestUser()                      //Fonction asynchrone, elle s'execute en parallele et peut se terminer plus tard, pour utilisé son contenu il faut utilisé "await"
+{
+    const test_user = {
+        username: client.inforUser.name,
+        password: "abcd*1234",
+        email: user.name + "@example.org"
+    };
+    console.log("Starting postUser");
+        const r = await fetch("http://127.0.0.1:8000/api/v1/auth/register", {     //adresse ip:port/ path du backend pour enregistrer un utilisateur
+            method: "POST",                                                       //Type de méthode utilisé pour le fetch
+            body: JSON.stringify(test_user),                                       //Convertie une string en .JSON
+            headers: {                                                            //Informations sur le contenu envoyé
+                "Accept": "application/json",
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(response =>  {
+            console.log("status =" + response.status);
+            console.log(JSON.stringify(test_user));
+            if (response.status >= 200 && response.status < 300)
+            {
+            }
+            else if (response.status === 400)
+            {
+                console.log("already registered");
+                return reponse.json();
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("On affiche le contenu qui est convertie depuis un JSON grace a response.json(): " + JSON.stringify(data));
+            localStorage.setItem("refresh", "refreshvalue");
+            localStorage.setItem("token", "tokenvalue");
+        })
+        .catch(error => {
+            console.error("postUser :", error);
+        });
 }
 
 function    getDataFromServer()
@@ -90,6 +170,7 @@ function    buttonConnection()
 
         // create_match('offline');
         // create_match('with friends');
+        loginTestUser();
         loadChat();
     });
 }
@@ -105,6 +186,7 @@ function    main()
     client.socket = socket;
 
     getDataFromServer();
+    registerTestUser();
     buttonConnection();
 }
 
