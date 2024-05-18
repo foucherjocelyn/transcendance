3b9cf30f2d06adfc44d57289de71c649e8addc5a
xuluu <2000xt2000@gmail.com>
2024-05-18 13:51:22 +0200

    change max point + check lost point in game

--- ft_transcendence/front_end/webServer/web/js/game/startGame.js
+++ ft_transcendence/front_end/webServer/web/js/game/startGame.js
@@ -56,7 +56,7 @@ function    setup_game_layer()
 
     gameLayerHTML();
     pongGame = new formPongGame();
-    pongGame.maxPoint = 10;
+    pongGame.maxPoint = 5;
     pongGame.listPlayer = match.listPlayer;
 
     setup_size_game_layer();
