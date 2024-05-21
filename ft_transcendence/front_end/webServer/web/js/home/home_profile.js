2781682b62dabe67e7a740028fb4853926188386
ctchen42 <ctchen42@student.42.fr>
2024-05-21 18:53:28 +0200

    inspect css + chatbox placement + img changed

--- ft_transcendence/front_end/webServer/web/js/home/home_profile.js
+++ ft_transcendence/front_end/webServer/web/js/home/home_profile.js
@@ -1,25 +1,20 @@
 import { getUserById, getUserIdByUsername } from "../backend_operation/get_user_info.js";
-import { noticeInvitePlayer } from "./home_game.js";
-import { upperPanel, upperPanelEventListener } from "./upper_panel.js";
 
 export async function	inspectProfile(username)
 {
 	let	user_id_obj = await getUserIdByUsername(username);
 	let user_info = await getUserById(user_id_obj.user_id);
-	document.getElementById("frontpage").outerHTML = `
-<div id="frontpage">
-	${upperPanel()}
+	document.getElementById("frontpage").insertAdjacentHTML("beforeend", `
 	<div id="inspectprofile">
+	<button id="inspectprofile_close" alt="a magnificient closing button"></button>
 	<img id="ip_avatar" src="../../img/${user_info.avatarPath}">
-	<p>Name = ${user_info.username}</p>
-	<p>Nb of game played = ${user_info.nb_games}</p>
-	<p>Average score = ${user_info.avg_score}</p>
-	<p>Level = ${user_info.level}</p>
+	<p>Name : ${user_info.username}</p>
+	<p>Nb of games : ${user_info.nb_games}</p>
+	<p>Average score : ${user_info.avg_score}</p>
+	<p>Level : ${user_info.level}</p>
 	</div>
-	<div id="chat"></div>
-	<div class="r_successinfo hide"></div>
-	${noticeInvitePlayer()}
-</div>
-`;
-	upperPanelEventListener();
+`);
+	document.getElementById("inspectprofile_close").addEventListener("click", () => {
+		document.querySelector("#inspectprofile").remove();
+	});
 }
