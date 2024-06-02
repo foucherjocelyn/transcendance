d71d4cd6fd02a4fe09295999943a2a32df94bbab
jfoucher <jfoucher@student.42.fr>
2024-06-02 23:38:47 +0200

    tournament tree updating bracket

--- ft_transcendence/front_end/webServer/web/js/tournamentTree/tournamentTree.js
+++ ft_transcendence/front_end/webServer/web/js/tournamentTree/tournamentTree.js
@@ -2,11 +2,14 @@ export const renderTournamentTree = (tournament) => {
     document.getElementById("tournament_tree").innerHTML = `
     <div class="bracket" id="bracket">
     </div>`;
-    const participants = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8"
+    const participants = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5"/*, "Player 6", "Player 7", "Player 8"
         /*"Player 9", "Player 10", "Player 11", "Player 12", "Player 13", "Player 14", "Player 15", "Player 16"*/
     ]; // Example participants
 
-    createBracket(tournament.player_usernames);
+    createBracket(participants);
+    //createBracket(tournament.player_usernames);
+    updateBracket(1, 1, "Player 1");
+    updateBracket(1, 2, "Player 4");
 }
 
 function createBracket(participants) {
@@ -45,7 +48,15 @@ function createBracket(participants) {
                 });
                 nextRoundParticipants.push('');
             } else {
-                matchDiv.innerHTML = `<div>${currentRoundParticipants[i]}</div><div>Bye</div>`;
+                matchDiv.innerHTML = `
+                <div class="team match-bye"><span class="name">${currentRoundParticipants[i]}</span></div>
+                <div class="match-lines">
+                    <div class="line one"></div>
+                    <div class="line two"></div>
+                </div>
+                <div class="match-lines alt">
+                    <div class="line one"></div>
+                </div>`;
                 nextRoundParticipants.push(currentRoundParticipants[i]);
             }
 
@@ -58,16 +69,14 @@ function createBracket(participants) {
 
 }
 
-function updateBracket(matchId, winner) {
-    const [round, match] = matchId.split('-').map(Number);
-    const nextRound = document.getElementById(`round${round + 1}`);
-    const nextMatch = nextRound ? nextRound.querySelector(`#match${round + 1}-${Math.ceil(match / 2)}`) : null;
-    if (nextMatch) {
-        const divs = nextMatch.querySelectorAll('div');
-        if (divs[0].textContent === 'Winner 1' || divs[0].textContent === 'Winner 2' || divs[0].textContent === '') {
-            divs[0].textContent = winner;
-        } else {
-            divs[1].textContent = winner;
-        }
+function updateBracket(round, match, winner) {
+    const matchDiv = document.getElementById(`match${round}-${match}`);
+    matchDiv.classList.add(`${matchDiv.querySelector(".match-top .name").innerText === winner ? "winner-top" : "winner-bottom"}`);
+    //if there is a next round, put the winner in the match of the next round
+    if (document.getElementById(`round${round + 1}`)) {
+        const nextMatch = Math.floor((match + 1) / 2);
+        const playerNextPosition = match % 2 ? "top" : "bottom";
+        const nextMatchDiv = document.getElementById(`match${round + 1}-${nextMatch}`);
+        nextMatchDiv.querySelector(`.match-${playerNextPosition} .name`).innerText = winner;
     }
 }
