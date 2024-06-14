6da6bee5dd28ba7fb9176a91230715bdae5905c0
ctchen42 <ctchen42@student.42.fr>
2024-06-14 18:32:19 +0200

    oauth 2 authorize login 42 wip

--- ft_transcendence/front_end/webSocket/js/webSocket/oauth2.js
+++ ft_transcendence/front_end/webSocket/js/webSocket/oauth2.js
@@ -1,10 +1,36 @@
-const oauth2 = require('simple-oauth2');
-const tokenConfig = {};
+require('dotenv').config();
 
+const config = {
+  client_id: process.env.FOURTWO_CLIENT_ID,
+  client_secret: process.env.FOURTWO_CLIENT_SECRET
+};
+
+const oauth2 = require('simple-oauth2').create(credentials);
+//const tokenConfig = {};
+
+export async function request42Login() {
+	try {
+		const response = await fetch(`https://api.intra.42.fr/oauth/authorize?client_id=${process.env.FOURTWO_CLIENT_ID}&redirect_uri=${redirect_uri}&scope=${scope}&state=${state}&response_type=${response}`, {
+			method: "GET"
+		})
+		if (!response.ok) {
+			console.log("request42Login: Client/Server error");
+			return;
+		}
+		const data = await response.json();
+		//console.log("request42Login:");
+		//console.log(data);
+		//return (data);
+	} catch (error) {
+		console.error("request42Login: ", error);
+	}
+}
+
+/*
 const credentials = {
     client: {
-        id: 'u-s4t2ud-5a9d7a791c31267b140be75dcb88368fd21ecc552a388ba8a2a2e5320d82015d',
-        secret: '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'
+        id: '',
+        secret: '',
     },
     auth: {
         tokenHost: 'https://api.intra.42.fr/oauth/token'
@@ -21,7 +47,46 @@ function requestOauth2() {
       const token = oauth2.accessToken.create(result);
     });
 }
+*/
+
+ /*
+// Authorization oauth2 URI
+const authorizationUri = oauth2.authorizationCode.authorizeURL({
+  redirect_uri: 'http://localhost:5500/callback',
+  scope: '<scope>',
+  state: '<state>'
+});
+ 
+// Redirect example using Express (see http://expressjs.com/api.html#res.redirect)
+res.redirect(authorizationUri);
+ 
+// Get the access token object (the authorization code is given from the previous step).
+const tokenConfig = {
+  code: '<code>',
+  redirect_uri: 'http://localhost:3000/callback'
+};
+ 
+// Callbacks
+// Save the access token
+oauth2.authorizationCode.getToken(tokenConfig, (error, result) => {
+  if (error) {
+    return console.log('Access Token Error', error.message);
+  }
+ 
+  const token = oauth2.accessToken.create(result);
+});
+ 
+// Promises
+// Save the access token
+oauth2.authorizationCode.getToken(tokenConfig)
+.then((result) => {
+  const token = oauth2.accessToken.create(result);
+})
+.catch((error) => {
+  console.log('Access Token Error', error.message);
+});
 
 module.exports = {
     requestOauth2
 };
+*/
